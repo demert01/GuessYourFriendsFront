0192666beae4bde79c198fcc5a14c61fbf707ee7
Nick Moeller <njmoeller@wisc.edu>
2021-03-19 11:57:34 -0500

    Adding four new files GameResult, QuestionScreen, RoundResult, RoundScreen and modified App.js accordingly

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./Home";
 import Signup from "./Signup";
 import Login from './Login';
+import RoundScreen from './RoundScreen';
+import QuestionScreen from './QuestionScreen';
+import RoundResults from './RoundResults';
+import GameResults from './RoundResults';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +19,10 @@ export default function App() {
           <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
           <Stack.Screen name="Signup" component={Signup} options={{ headerShown: true }} />
           <Stack.Screen name="Login" component={Login} options={{ headerShown: true }} />
+          <Stack.Screen name="RoundScreen" component={RoundScreen} options={{ headerShown: true }} />
+          <Stack.Screen name="QuestionScreen" component={QuestionScreen} options={{ headerShown: true }} />
+          <Stack.Screen name="RoundResults" component={RoundResults} options={{ headerShown: true }} />
+          <Stack.Screen name="GameResults" component={GameResults} options={{ headerShown: true }} />
         </Stack.Navigator>
       </NavigationContainer>
   );
