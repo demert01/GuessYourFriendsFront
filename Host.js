04e4af3b9b63152a2d705929502d8110cfd8b3fa
Nick Moeller <njmoeller@wisc.edu>
2021-03-25 13:08:32 -0500

    Changes to host

--- Host.js
+++ Host.js
@@ -1,100 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import ButtonWithBackground from "./button";
-import { StyleSheet, Text, View, ImageBackground} from 'react-native';
-
-
-
-
-// Displays Host Screen
-export default class Host extends React.Component {
-  render() {
-    const { navigate } = this.props.navigation;
-    return (  
-        <View style={styles.container}>
-          <View style={styles.carContainer}>
- 
-            <ImageBackground 
-                source={require('./assets/background.png')}
-                style={styles.image}
-            />
-
-              <View style={styles.titles}>
-                <Text style={styles.title}>Guess Your Friends</Text>
-                <Text style={styles.subtitle}>Placeholder</Text>
-              </View>
-
-              <View style={styles.importantText}>
-                <Text style={styles.instruction}>Your Lobby Code:</Text>
-              </View>
-
-            <View style={styles.button}>
-              <ButtonWithBackground onPress={() => {navigate('RoundScreen')}} text='Start the Game!' color = '#000000'/>         
-            </View>
-          </View>
-          <StatusBar style="auto" />
-          
-        </View>
-      
-    );
+class Host {
+  constructor(joinCode, ready, start) {
+      // device ids contains array of all nicknames of game participants
+      this.joinCode = joinCode;
+      this.ready = ready;
+      this.start = start;
   }
 }
 
-// Formatting
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
-
-  titles: {
-    width: '100%',
-    alignItems: 'center',
-    //backgroundColor: '#d12a3b'
-  },
-
-  title: {
-    fontSize: 40,
-    fontWeight: '700',
-    color: 'white'
-  },
-  
-  subtitle: {
-    fontSize: 16,
-    color: 'white',
-    fontWeight: '700'
-  },
-
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute'
-  },
-
-  importantText: {
-    marginTop: '10%',
-    width: '100%',
-    alignItems: 'center'
-  },
-
-  instruction: {
-    fontSize: 30,
-    fontWeight: '700',
-    color: 'white',
-    marginTop: 27
-  },
-
-  button: {
-    marginTop: '60%',
-    alignItems: 'center',
-    color: '#000000'
-  }
-});
+export default Host
