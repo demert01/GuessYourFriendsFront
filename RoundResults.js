8f9a14d418d68659a1ef00b75b68a2dcc9856d58
Nick Moeller <njmoeller@wisc.edu>
2021-04-08 17:07:17 -0500

    Created round results page

--- RoundResults.js
+++ RoundResults.js
@@ -1,55 +1,83 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, Text, View} from "react-native";
+import {ImageBackground, StyleSheet, Text, View, ScrollView, ActivityIndicator, RefreshControl} from "react-native";
 import ButtonWithBackground from "./button";
 import {StatusBar} from "expo-status-bar";
+const GameAPI = require('./API/Game/GameAPI');
 
-class RoundResults extends React.Component {
-    
-    // Navigates to Submit Order
-    moveToSubmitOrder = (paymentType) => {
-        this.props.navigation.navigate('SubmitOrder', { cart: this.props.route.params.cart, paymentType: paymentType })
-    };
+class QuestionScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currQuestionNumber: 0,
+            currRoundNumber: 1,
+            //players: this.props.route.params.players,
+            disableButtons: false,
+            loading: false,
+            makingAPICall: false,
+            waitingForNext: false
+        }
+    }
 
+    checkRoundNumber(){
+        if(this.state.currQuestionNumber % 5 == 0 && this.state.currQuestionNumber != 0){
+            this.state.currRoundNumber = this.state.currRoundNumber + 1;
+        }
+    }
 
-    // Checkout Screen which includes four payment options
+    // Round Results will display the results for each qeustion
     render() {
+        //const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                <View style={styles.carContainer}>
-
-                    <ImageBackground
+                <ImageBackground
                         source={require('./assets/background.png')}
                         style={styles.image}
-                    />
+                />
 
+                <View style={styles.questionContainer}>   
                     <View style={styles.titles}>
-                        <Text style={styles.title}>Badger Bytes</Text>
-                        <Text style={styles.subtitle}>Get Your Grub On</Text>
+                        {/* Must increment round every every time questionNumber % 5 == 0 */}
+                            {this.checkRoundNumber()}
+                            <Text style={styles.title}>Round {this.state.currRoundNumber}</Text>
+                        </View>
+
+                    
+                    <View style={styles.titles2}>
+                        <Text style={styles.instruction}>Results from Question {this.state.currQuestionNumber + 1}</Text>
                     </View>
 
                     <View style={styles.importantText}>
-                        <Text style={styles.instruction}>Select Your Payment Method</Text>
+                        <Text style={styles.question}>Display Question Here</Text>
                     </View>
 
-                    <View style={styles.buttons}>
-                        <ButtonWithBackground onPress={() => this.moveToSubmitOrder("Credit Card")} text='Credit Card' color='#d12a3b' />
-                    </View>
+                    <ScrollView refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={this._onRefresh}
+                        />
+                    }>
+                        {/*{ This will display the players in the game along with their respective vote counts for a question
+                            this.state.nicknames.map((item, index) => (
+                                <View key = {index} style = {styles.item}>
+                                    <Text style={{fontSize: 25, fontWeight: '800', color: 'white'}}>{item}</Text>
+                                </View>
+                            ))
+                        }
+                    */}
+                     </ScrollView>
 
-                    <View style={styles.buttons}>
-                        <ButtonWithBackground onPress={() => this.moveToSubmitOrder("PayPal")} text='Paypal' color='#d12a3b' />
-                    </View>
-
-                    <View style={styles.buttons}>
-                        <ButtonWithBackground onPress={() => this.moveToSubmitOrder("Stripe")} text='Stripe' color='#d12a3b' />
-                    </View>
-
-                    <View style={styles.buttons}>
-                        <ButtonWithBackground onPress={() => this.moveToSubmitOrder("ApplePay")} text='ApplePay' color='#d12a3b' />
-                    </View>
+                        {this.state.loading &&
+                        <View style={styles.loading}>
+                            <Text style={styles.loadingText}>Waiting for Other Players</Text>
+                            <ActivityIndicator/>
+                        </View>
+                        }
 
                 </View>
+
                 <StatusBar style="auto" />
             </View>
+            
         );
     }
 }
@@ -63,31 +91,33 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
-    carContainer: {
-        width: '100%',
+    questionContainer: {
         height: '100%',
+        width: '100%',
+        padding: 10,
     },
 
     buttons: {
         marginTop: '10%',
         alignItems: 'center',
-    },
-
-    button2: {
-        marginTop: '5%',
-        alignItems: 'center',
-
+        height: '100%'
     },
 
     titles: {
         width: '100%',
         alignItems: 'center',
-        backgroundColor: '#d12a3b'
+        paddingTop: 10
+    },
+
+    titles2: {
+        width: '100%',
+        alignItems: 'center',
+        marginTop: 20
     },
 
     title: {
         fontSize: 35,
-        fontWeight: '700',
+        fontWeight: '900',
         color: 'white',
         textAlign: 'center'
     },
@@ -106,19 +136,44 @@ const styles = StyleSheet.create({
     },
 
     importantText: {
-        marginTop: '5%',
+        marginTop: '20%',
         width: '100%',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'center'
       },
     
     instruction: {
-        fontSize: 25,
-        fontWeight: '700',
+        fontSize: 30,
+        fontWeight: '800',
         color: 'white',
-        marginTop: 27
-      }
+        textAlign: 'center'
+      },
 
+    question: {
+        fontSize: 30,
+        fontWeight: '800',
+        color: 'white',
+        textAlign: 'center'
+      },
+
+    loading: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+       // opacity: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    loadingText:  {
+        fontSize: 25,
+        fontWeight: '800',
+        color: 'black',
+        marginTop: 5,
+        textAlign: 'center'
+    },
 
 });
-
-export default RoundResults
+export default QuestionScreen
